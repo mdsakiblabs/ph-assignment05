660badb58b7def43d76bd8e4a905c0359f31c35a
sakib hasan <mdsakibhasanankon@gmail.com>
2026-09-13 23:09:19 +0600

    fix button alignment in card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,30 +10,41 @@ interface CardProps {
 }
 
 const Card = (props: CardProps) => {
-  console.log(props.onAdd)
-
   return (
-    <div className="container border border-[#f1f1f1] rounded-2xl px-4 py-3">
+    <div className="container border border-[#f1f1f1] rounded-2xl px-4 py-3 flex flex-col">
       <div className="flex justify-between">
-        <img src={props.icon} alt="" className="w-8"/>
-        <h2 className="bg-[#595959]  text-white font-light  flex justify-center items-center px-2 rounded-full py-0 text-[14px]">{props.badge}</h2>
+        <img src={props.icon} alt="" className="w-8" />
+
+        <h2 className="bg-[#595959] text-white font-light flex justify-center items-center px-2 rounded-full py-0 text-[14px]">
+          {props.badge}
+        </h2>
       </div>
 
       <div className="flex flex-col gap-4 mb-5 mt-4">
-        <h1 className="text-2xl font-medium">{props.name}</h1> 
+        <h1 className="text-2xl font-medium">{props.name}</h1>
+
         <p className="font-light">{props.description}</p>
-        </div>
+      </div>
 
       <div className="flex justify-between mb-10 text-[13px] font-medium">
-        <h3 className="bg-[#475569] rounded text-white py-0.5 px-0.75">{props.category}</h3>
-        <h2>{props.difficulty}</h2>
-        <h2 className="flex items-center gap-0.5">
-            <span className="text-yellow-400 text-[15px]">★</span>{props.rating}</h2>
-        </div>
+        <h3 className="bg-[#475569] rounded text-white py-0.5 px-0.75">
+          {props.category}
+        </h3>
 
-      <button 
-      onClick={props.onAdd}
-      className="text-center font-medium text-[17px] text-white bg-[#1f1f1f] w-full rounded py-1 cursor-pointer">Add To Stack</button>
+        <h2>{props.difficulty}</h2>
+
+        <h2 className="flex items-center gap-0.5">
+          <span className="text-yellow-400 text-[15px]">★</span>
+          {props.rating}
+        </h2>
+      </div>
+
+      <button
+        onClick={props.onAdd}
+        className="mt-auto text-center font-medium text-[17px] text-white bg-[#1f1f1f] w-full rounded py-1 cursor-pointer"
+      >
+        Add To Stack
+      </button>
     </div>
   );
 };
